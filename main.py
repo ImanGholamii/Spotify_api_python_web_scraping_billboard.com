16ce440262ebc8d3f2c26aec266c737ef7eee20f
Iman <imangholamiimi@gmail.com>
2024-10-07 19:21:31 +0330

    :sparkles: | ADD | date validation for user input

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+from datetime import datetime
+
 from bs4 import BeautifulSoup
 from requests import get
 
+
+def validate_date_format(date_string, format):
+    try:
+        date_parts = date_string.split('-')
+        year, month, day = date_parts[0], date_parts[1], date_parts[2]
+        if len(year) == 4 and len(month) == 2 and len(day) == 2:
+            datetime.strptime(date_string, format)
+            return True
+    except ValueError:
+        return False
+
+
+needs_to_try = True
+while needs_to_try:
+    date_string = input("Which date do you want to travel? type date in YYYY-MM-DD format:")
+    if validate_date_format(date_string, "%Y-%m-%d"):
+        needs_to_try = False
+    else:
+        needs_to_try = True
+
 URL = "https://www.billboard.com/charts/hot-100/"
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                  ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0'
 }
 response = get(url=URL, headers=headers)
 response.raise_for_status()
@@ -12,8 +33,7 @@ html_page = response.text
 
 soup = BeautifulSoup(markup=html_page, features='html.parser')
 all_titles = soup.find_all(name='h3', class_='c-title')
-song_titles = []
-for title in all_titles[7:404:2]:
-    title = title.get_text().replace('Producer(s):', '')
-    song_titles.append(title.strip())
+titles = [title.getText().replace('Producer(s):', '') for title in all_titles[7:404:2]]
+song_titles = [title.strip() for title in titles]
+
 song_titles = song_titles[::2]
