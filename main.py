424a76b2da04565baacf82a6eb5be9b9431e0454
Iman <imangholamiimi@gmail.com>
2024-10-08 13:04:36 +0330

    :sparkles: | ADD | decuple to use spotify secrets and | :zap: | updated gitignore and requirements

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from datetime import datetime
 
 from bs4 import BeautifulSoup
+from decouple import config
 from requests import get
 
+CLIENT_ID = config('CLIENT_ID')
+CLIENT_SECRET = config('CLIENT_SECRET')
 
-def validate_date_format(date_string, format):
+
+def validate_date_format(date_string, format_):
     try:
         date_parts = date_string.split('-')
         year, month, day = date_parts[0], date_parts[1], date_parts[2]
         if len(year) == 4 and len(month) == 2 and len(day) == 2:
-            datetime.strptime(date_string, format)
+            datetime.strptime(date_string, format_)
             return True
     except ValueError:
         return False
@@ -17,16 +21,18 @@ def validate_date_format(date_string, format):
 
 needs_to_try = True
 while needs_to_try:
-    date_string = input("Which date do you want to travel? type date in YYYY-MM-DD format:")
-    if validate_date_format(date_string, "%Y-%m-%d"):
+    date = input("Which date do you want to travel? type date in YYYY-MM-DD format:")
+    if validate_date_format(date_string=date, format_="%Y-%m-%d"):
         needs_to_try = False
     else:
         needs_to_try = True
 
-URL = "https://www.billboard.com/charts/hot-100/"
+URL = "https://www.billboard.com/charts/hot-100/" + date
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                  ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
 }
+
 response = get(url=URL, headers=headers)
 response.raise_for_status()
 html_page = response.text
@@ -37,3 +43,4 @@ titles = [title.getText().replace('Producer(s):', '') for title in all_titles[7:
 song_titles = [title.strip() for title in titles]
 
 song_titles = song_titles[::2]
+print(song_titles)
